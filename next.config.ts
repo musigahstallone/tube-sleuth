c08b54afbcd23de356f3d76fa47200379f5b3540
Musigah Stallone <musigahstallone@gmail.com>
2025-09-03 09:50:10 +0000

    Error: Invalid src prop (https://i.ytimg.com/vi/vRnkwuG5dts/mqdefault.jp

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
